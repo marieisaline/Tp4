9ce5ab11a15923fc262fd04ff0a0c63dcb42e268
Bontemps <miabontemps@gmail.com>
2024-10-20 19:05:19 +0200

    question9

--- javawork/TP4/src/fr/univ_smb/iae/tp4/bontmari/bulletins/BulletinMeteo.java
+++ javawork/TP4/src/fr/univ_smb/iae/tp4/bontmari/bulletins/BulletinMeteo.java
@@ -26,7 +26,7 @@ public class BulletinMeteo extends Bulletin {
 	    // Méthode pour obtenir la zone géographique associée au bulletin météo
 	    public String getZone_geo() {
 	        return this.zone_geo;
-	    }   
+	    }
 
 	    // Méthode toString pour représenter l'objet BulletinMeteo sous forme de chaîne de caractères
 	    @Override
@@ -35,7 +35,9 @@ public class BulletinMeteo extends Bulletin {
 	        return super.toString() + " (" + this.zone_geo + ")"; // Appel de toString() de Bulletin
 	    }
 
-   
+	    
+	    
+	    
 	    //Question 6 
 	    public static BulletinMeteo randomBulletinMeteo() {
 	        String[] tempsQuilFait = {"Grand beau temps", "Pluie", "Quelques averses", "Brouillard givrant", "Vent fort", "Nuageux"};
@@ -110,4 +112,3 @@ public class BulletinMeteo extends Bulletin {
 	        // Retourne la liste des 10 bulletins météo générés
 	        //return bulletins;
 	    }
-	
